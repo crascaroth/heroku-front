041acaae3dd10b744a19924004733da80f2ea01a
DouglasFlorido <crascaroth@gmail.com>
2021-09-18 15:26:34 -0300

    todos os arquivos front na root

---
+++ src/Pages/MainPage/MainPage.js
@@ -0,0 +1,18 @@
+import { useContext } from 'react'
+import Context from '../../GlobalState/Context'
+
+
+const MainPage = () => {
+
+    const { states, setters, requests } = useContext(Context)
+
+    
+
+    return (
+        <div>
+            {}
+        </div>
+    )
+}
+
+export default MainPage
